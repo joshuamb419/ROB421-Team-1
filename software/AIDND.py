236c43814d85939aa6fdd4a793fa3b5b6a7968f9
Joshua Brown <josh.bikedude.419@gmail.com>
2025-05-14 12:09:41 -0700

    prompt update

--- software/AIDND.py
+++ software/AIDND.py
@@ -1,7 +1,7 @@
 import ollama
 
 MODEL = "gemma3:1b"
-PROMPT = "You are a dungeon master in charge of running a dnd session. You are running a short session where the players raid a bandit camp. Be sure to ask the players what they would like to do, never assume an action that they take. The players are here and ready to start the session, please begin by narrating the opening scene and asking the players what they want to do."
+PROMPT = "You are a dungeon master in charge of running a dnd session. You are running a short session where the players raid a bandit camp. Be sure to ask the players what they would like to do, never assume an action that they take. The players are here and ready to start the session, please begin by narrating the opening scene and asking the players what they want to do. Additionally you may express and gesture by placing emotes from the following list in square brackets like this. `[surprise]` You may only choose from this list: concert, double_wave, vibe, wave, surprise"
 
 with open("ollama_credentials", 'r') as file:
     credentials = file.readline()
