19514778666594945cc315b25a867a49c463e7c7
Bierlyl <156222338+Bierlyl@users.noreply.github.com>
2025-05-29 11:08:27 -0700

    Update Record.py to hold push to talk

--- software/AIDnd/Record.py
+++ software/AIDnd/Record.py
@@ -2,8 +2,16 @@
 
 import pyaudio
 import wave
+import Keyboard as Key
+import time
 
-def record(chunk=1024, sample_format = pyaudio.paInt16, channels=2, fs=44100, seconds=3, filename="input.wav"):
+def key_press(hotkey):
+    if Key.is_pressed(hotkey):
+        return True
+    else:
+        return False
+
+def record(chunk=1024, sample_format = pyaudio.paInt16, channels=2, fs=44100, seconds=3, filename="input.wav", hotkey = 'space'):
     p = pyaudio.PyAudio()  # Create an interface to PortAudio
 
     stream = p.open(format=sample_format,
@@ -12,29 +20,32 @@ def record(chunk=1024, sample_format = pyaudio.paInt16, channels=2, fs=44100, se
                     frames_per_buffer=chunk,
                     input=True)
     
-    print('Recording')
+    print(f'Press {hotkey} to begin listening')
 
     frames = []  # Initialize array to store frames
 
     # Store data in chunks
-    try:
-        while True:
-            data = stream.read(chunk)
-            frames.append(data)
-    except KeyboardInterrupt:
-        # Stop and close the stream 
-        stream.stop_stream()
-        stream.close()
+    Key.wait(hotkey)
+    time.sleep(0.1)
+    print(listening)
+    while key_press(hotkey):
+        data = stream.read(chunk)
+        frames.append(data)
+        
+    print('Listening Stopped')
+    
+    # Stop and close the stream 
+    stream.stop_stream()
+    stream.close()
 
-        # Terminate the PortAudio interface
-        p.terminate()
-        print('Finished recording')
-
-        # Save the recorded data as a WAV file
-        wf = wave.open(filename, 'wb')
-        wf.setnchannels(channels)
-        wf.setsampwidth(p.get_sample_size(sample_format))
-        wf.setframerate(fs)
-        wf.writeframes(b''.join(frames))
-        wf.close()
+    # Terminate the PortAudio interface
+    p.terminate()
+    
+    # Save the recorded data as a WAV file
+    wf = wave.open(filename, 'wb')
+    wf.setnchannels(channels)
+    wf.setsampwidth(p.get_sample_size(sample_format))
+    wf.setframerate(fs)
+    wf.writeframes(b''.join(frames))
+    wf.close()
         
